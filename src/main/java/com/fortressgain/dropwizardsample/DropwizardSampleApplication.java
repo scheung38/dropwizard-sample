30b626b950cf74b6192d5ef8549cc2e2198dc48d
Sebastian Cheung <sebastian_cheung@yahoo.com>
2017-06-23 00:36:24 +0100

    Initial commit

---
+++ src/main/java/com/fortressgain/dropwizardsample/DropwizardSampleApplication.java
@@ -0,0 +1,13 @@
+package com.fortressgain.dropwizardsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropwizardSampleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(DropwizardSampleApplication.class, args);
+	}
+}
